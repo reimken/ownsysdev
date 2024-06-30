11adf9579a862ec048cb9300187d340597866a90
Regenye Imre <regenyeimi@gmail.com>
2024-06-30 17:55:49 +0200

    Adatbzissal kiegészítve, 2 mérföldkő vége

--- FlaskWebProject2/WebApp/__init__.py
+++ FlaskWebProject2/WebApp/__init__.py
@@ -1,13 +1,22 @@
 from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))   #megadja a projekt akutális elérési útját
+
 app = Flask(__name__)
-from WebApp import routes
+#required for web forms
+app.config["SECRET_KEY"] = "rendszerfejlesztes"
 
 
-'''#required for SQLAlchemy
+#required for SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL') or \
- 'sqlite:///' + os.path.join(basedir, 'app.db')
+ 'sqlite:///' + os.path.join(basedir, 'sysdevdb.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-from WebApp import routes, models'''
+
+from WebApp import routes, models
+
